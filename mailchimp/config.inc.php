0ff08d96f9ba6c680cd2d5bcef2e4037ba6af0d0
tobylunt <toby.lunt@gmail.com>
2015-10-02 18:08:19 +0200

    mail chimp updates
    hope!

---
+++ mailchimp/config.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+// API Key - see http://admin.mailchimp.com/account/api
+$apikey			= 'b9c46aa1895373866b3fdb257d2eb044-us11'; // YOUR MAILCHIMP APIKEY
+    
+// A List Id to run examples against. use lists() to view all
+// Also, login to MC account, go to List, then List Tools, and look for the List ID entry
+$listId			= '07b06e9ee5'; // YOUR MAILCHIMP LIST ID - see lists() method
+    
+// A Campaign Id to run examples against. use campaigns() to view all
+$campaignId		= ''; // YOUR MAILCHIMP CAMPAIGN ID - see campaigns() method
+
+// just used in xml-rpc examples
+$apiUrl			= 'http://api.mailchimp.com/1.3/';
+    
+?>
